5a9543b0f45aa89ce61edfd686ca2032be6e21db
mff5 <166507311+mff5@users.noreply.github.com>
2026-02-17 14:07:28 +0900

    model_factory.py 단순화

--- src/oxford_pet_model_comparison/models/model_factory.py
+++ src/oxford_pet_model_comparison/models/model_factory.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 import torch.nn as nn
 
 from .simple_cnn import build_simple_cnn
@@ -7,13 +5,6 @@ from .resnet18 import build_resnet18
 from .efficientnet_b0 import build_efficientnet_b0
 from .mobilenet_v2 import build_mobilenet_v2
 
-
-MODEL_REGISTRY: dict[str, Callable[..., nn.Module]] = {
-    "simple_cnn": build_simple_cnn,
-    "resnet18": build_resnet18,
-    "efficientnet_b0": build_efficientnet_b0,
-    "mobilenet_v2": build_mobilenet_v2,
-}
 
 def build_model(
     model_name: str,
@@ -24,13 +15,12 @@ def build_model(
 
     model_name = model_name.lower()
 
-    build_fn = MODEL_REGISTRY[model_name]
 
     if model_name == "simple_cnn":
-        return build_fn(num_classes=num_classes)
-
-    return build_fn(
-        num_classes=num_classes,
-        pretrained=pretrained,
-        freeze_backbone=freeze_backbone,
-    )
+        return build_simple_cnn(num_classes=num_classes)
+    if model_name == "mobilenet_v2":
+        return build_mobilenet_v2(num_classes=num_classes, pretrained=pretrained, freeze_backbone=freeze_backbone)
+    if model_name == "efficientnet_b0":
+        return build_efficientnet_b0(num_classes=num_classes, pretrained=pretrained, freeze_backbone=freeze_backbone)
+    if model_name == "resnet18":
+        return build_resnet18(num_classes=num_classes, pretrained=pretrained, freeze_backbone=freeze_backbone)
