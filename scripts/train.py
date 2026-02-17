73ba7ac263208c74f0c444778142cfb9e7b43c2d
mff5 <166507311+mff5@users.noreply.github.com>
2026-02-17 11:05:39 +0900

    train.py

--- scripts/train.py
+++ scripts/train.py
@@ -3,25 +3,38 @@ from pathlib import Path
 import hydra
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
+from omegaconf import OmegaConf
 
 import torch
 import torch.nn as nn
 
-from oxford_pet_model_comparison.engine import fit, build_training_components
+from oxford_pet_model_comparison.engine import Trainer, build_training_components
 from oxford_pet_model_comparison.models import build_model
 from oxford_pet_model_comparison.datasets import build_dataloaders
 
 
-def run_one_exp(cfg: DictConfig, device: torch.device) -> None:
-    run_name = cfg.exp.name
+def run_one_exp(
+        cfg: DictConfig,
+        device: torch.device
+) -> None:
 
-    run_dir = Path(HydraConfig.get().runtime.output_dir)
-    print(f"run_dir: {run_dir}")
+    runs_dir = Path(cfg.paths.runs_dir)
+
+    exp_name = cfg.exp.name
+
+    run_dir = runs_dir / exp_name
+    run_dir.mkdir(exist_ok=True, parents=True)
+    OmegaConf.save(cfg, run_dir / "config.yaml")
+
+
+    if device.type == "cuda":
+        torch.backends.cudnn.benchmark = True
+        torch.set_float32_matmul_precision("high")
 
     train_loader, val_loader, _ = build_dataloaders(cfg)
 
     model = build_model(
-        cfg.model.name,
+        model_name=cfg.model.name,
         num_classes=cfg.dataset.num_classes,
         pretrained=cfg.model.pretrained,
         freeze_backbone=cfg.model.freeze_backbone
@@ -36,23 +49,28 @@ def run_one_exp(cfg: DictConfig, device: torch.device) -> None:
         device=device,
     )
 
-    fit(
-        run_name=run_name,
-        run_dir=run_dir,
-        num_epochs=cfg.train.num_epochs,
+    trainer = Trainer(
         model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
         loss_fn=loss_fn,
         device=device,
         optimizer=components.optimizer,
         scaler=components.scaler,
-        scheduler=components.scheduler,
+        scheduler=components.scheduler
+    )
+
+    trainer.fit(
+        exp_name=exp_name,
+        run_dir=run_dir,
+        num_epochs=cfg.train.num_epochs,
+        train_loader=train_loader,
+        val_loader=val_loader
     )
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="config")
-def main(cfg: DictConfig) -> None:
+def main(
+        cfg: DictConfig
+) -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"RUN: {cfg.exp.name}")
     run_one_exp(cfg, device=device)
